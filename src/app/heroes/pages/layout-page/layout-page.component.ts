2221e472671943b11b04442b8a8f61f6fbfb142f
sebas524 <104307242+sebas524@users.noreply.github.com>
2023-04-12 12:46:45 +0200

    add navbar and sidebar

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
 
+interface sidebarItems {
+  label: string;
+  icon: string;
+  url: string;
+}
+
 @Component({
   selector: 'app-layout-page',
   templateUrl: './layout-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class LayoutPageComponent {
-
+  public myItems: sidebarItems[] = [
+    {
+      label: 'List',
+      icon: 'label',
+      url: './list',
+    },
+    {
+      label: 'Add',
+      icon: 'add',
+      url: './new-hero',
+    },
+    {
+      label: 'Search',
+      icon: 'search',
+      url: './search',
+    },
+  ];
 }
