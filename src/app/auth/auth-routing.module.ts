1f68385940c7341e70eefe1036510ffaf81233af
sebas524 <104307242+sebas524@users.noreply.github.com>
2023-04-12 09:50:12 +0200

    first commit lazy load routes set

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,25 @@
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      { path: 'login', component: LoginPageComponent },
+      { path: 'sign-up', component: LoginPageComponent },
+      {
+        path: '**',
+        redirectTo: 'login',
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AuthRoutingModule {}
---
+++ src/app/auth/pages/signup-page/signup-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-page',
+  templateUrl: './signup-page.component.html',
+  styles: [
+  ]
+})
+export class SignupPageComponent {
+
+}
